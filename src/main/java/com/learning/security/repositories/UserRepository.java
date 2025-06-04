dd60933d4e71584dcec21e0041dc163b004a8ca5
Amanda Evellin <amandasousav19@gmail.com>
2025-06-04 20:57:55 -0300

    Implementação base de usuário

---
+++ src/main/java/com/learning/security/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.learning.security.repositories;
+
+import com.learning.security.models.entities.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+  UserDetails findByLogin(String login);
+}
